78e4321b330e032a6402f4dfcd115fb338cfccf8
vardhan29122003 <vardhanyadlapalli225@gmail.com>
2025-08-12 21:24:10 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FUTURE_FS_01/', // 👈 important for GitHub Pages
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
